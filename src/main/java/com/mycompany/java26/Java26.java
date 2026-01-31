7b07bdc992efa8c9e20f7625427e1637dcc0c7a0
orion-kendrix <mishrashivesh729@gmail.com>
2026-01-31 17:19:39 +0530

    1st version
    Added difficulty picker using choice.
    Coded the "Point guesser game" using the ThreadLocalRandom from concurrent.

--- src/main/java/com/mycompany/java26/Java26.java
+++ src/main/java/com/mycompany/java26/Java26.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.java26;
 
 /**
@@ -9,46 +8,59 @@ package com.mycompany.java26;
  * @author BUDDY
  */
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Java26 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Random rand = new Random();
-        int randomX = rand.nextInt(100);
-        int randomY = rand.nextInt(100);
         
-        System.out.println("Guess the X position: ");
-        int guessX = Integer.valueOf(scan.nextLine());
-        System.out.println("Guess the Y position");
-        int guessY = Integer.valueOf(scan.nextLine());
-        //Comment for the trial push in github...
-        
-        do {
-            if ((guessX == randomX) || (guessY < randomY)) {
-                System.out.println("STOP!! go up");
-            }
-            else if ((guessX == randomX) || (guessY > randomY)) {
-                System.out.println("STOP!! go down");
-            }
-            else if ((guessY == randomY) || (guessX < randomX)) {
-                System.out.println("STOP! go straight forward");
-            }
-            else if ((guessY == randomY) || (guessX > randomX)) {
-                System.out.println("STOP! go straight back");
-            }
-            else if ((guessX < randomX) || (guessY < randomY)) {
-                System.out.println("STOP! go down... ");
-            }
-            else if ((guessX < randomX) || (guessY > randomY)) {
-                System.out.println("STOP! go down... ");
-            }
-            else if ((guessX > randomX) || (guessY < randomY)) {
-                System.out.println("STOP! go down... ");
-            }
-            else if ((guessX > randomX) || (guessY > randomY)) {
-                System.out.println("STOP! go down... ");
-            }     
+        int randomX = ThreadLocalRandom.current().nextInt(-100, 101);
+        int randomY = ThreadLocalRandom.current().nextInt(-100, 101);
+        int attempts = 0;
+
+        System.out.println("Choose the difficulty:\n1 1.Easy\n2 2.Medium\n3 3.Hard"); //Difficulty Option
+        int diff = Integer.parseInt(scan.nextLine());
+        switch (diff) {
+            case 1 -> attempts = 30;
+            case 2 -> attempts = 20;
+            case 3 -> attempts = 10;
+            default -> System.out.println("Invalid Choice!");
         }
         
+        do { 
+            System.out.println("Chances left:  " + attempts);
+            System.out.println("Guess the X position: ");
+            int guessX = Integer.parseInt(scan.nextLine());
+            System.out.println("Guess the Y position");
+            int guessY = Integer.parseInt(scan.nextLine());                                              //Game
+            if ((guessX == randomX) || (guessY < randomY)) {
+                System.out.println("STOP!! go up");
+                attempts--;
+            } else if ((guessX == randomX) || (guessY > randomY)) {
+                System.out.println("STOP!! go down");
+                attempts--;
+            } else if ((guessY == randomY) || (guessX < randomX)) {
+                System.out.println("STOP! go straight forward");
+                attempts--;
+            } else if ((guessY == randomY) || (guessX > randomX)) {
+                System.out.println("STOP! go straight back");
+                attempts--;
+            } else if ((guessX < randomX) || (guessY < randomY)) {
+                System.out.println(">^");
+                attempts--;
+            } else if ((guessX < randomX) || (guessY > randomY)) {
+                System.out.println(">!");
+            } else if ((guessX > randomX) || (guessY < randomY)) {
+                System.out.println("^<");
+                attempts--;
+            } else if ((guessX > randomX) || (guessY > randomY)) {
+                System.out.println("!<");
+                attempts--;
+            } else if ((guessX > randomX) || (guessY > randomY)) {
+                System.out.println("You win!! Hooray!!");
+            }
+        } while (attempts > 0);
+        System.out.println("You lose!");
     }
 }
